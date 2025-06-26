12a6ee812eba2f243b314d51ba645476bfde73b3
ysnbl <yassine.belahrache@gmail.com>
2025-06-26 08:54:22 +0300

    Update storeinfo.py

--- storeinfo.py
+++ storeinfo.py
@@ -93,4 +93,6 @@ TEXT:
         return jsonify({"error": str(e), "raw": response}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
